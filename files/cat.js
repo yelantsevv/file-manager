93ff3c9b21237270ed9f514a1bd77199fe4c7b42
yelantsevv <yelantsevv@gmail.com>
2024-10-06 20:29:19 +0300

    cat

---
+++ files/cat.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+
+function cat(__currentDir, path_to_file) {
+  const filePath = path.join(__currentDir, `${path_to_file}`);
+
+  const readableStream = fs.createReadStream(filePath, { encoding: "utf8" });
+
+  readableStream.on("data", (chunk) => {
+    console.log(chunk);
+  });
+
+  readableStream.on("end", () => {
+    console.log("File reading completed.");
+  });
+
+  readableStream.on("error", (err) => {
+    console.error("Error reading file:", err.message);
+  });
+}
+export { cat };
